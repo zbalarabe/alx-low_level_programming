82048df5532bffbade0a266de3ca6988d3c70ca3
“Zaharaddeen <zaharaddeenb@gmail.com>
2022-12-11 22:42:22 +0000

    Updated error!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
-int main()
+/* This progam prints using puts */
+
+int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 
-	return 0;
+	return (0);
 }
