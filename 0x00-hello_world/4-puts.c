9ff91eabb2876ea8d26ccf012263d451e5e35c7a
“Zaharaddeen <zaharaddeenb@gmail.com>
2022-12-12 16:17:34 +0000

    C program fixed.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - entry point 
+ * main - main block 
  * 
- * return 0
+ * return: 0
  */
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
