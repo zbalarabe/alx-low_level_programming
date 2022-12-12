4c2f75317727168fd2c13905fb5beec354ec866b
“Zaharaddeen <zaharaddeenb@gmail.com>
2022-12-12 22:59:33 +0000

    Program prints all single digit numbers of base 10 starting from 0.

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Program Starts
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
