0c7b80c4aae6b89c2c59509e6ff9c9049ab6833d
“Zaharaddeen <zaharaddeenb@gmail.com>
2022-12-12 22:43:21 +0000

    Program prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Program Starts
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
+
