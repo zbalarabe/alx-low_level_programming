c2057560b20539d27ed24bad0d29672274028c44
“Zaharaddeen <zaharaddeenb@gmail.com>
2022-12-15 03:40:58 +0000

    Prints the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void);
+
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+
+}
