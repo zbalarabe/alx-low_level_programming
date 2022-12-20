5c93481457ab076dc6591a188767f6fe290fb591
“Zaharaddeen <zaharaddeenb@gmail.com>
2022-12-20 23:20:47 +0000

    Function fixed

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98
+ * reset_to_98 - takes a pointer to an int as parameter and updates the value
  * @n: integer
  * Return: 0
- * /
+ */
 
 void reset_to_98(int *n)
+
 {
 	*n = 98;
 }
