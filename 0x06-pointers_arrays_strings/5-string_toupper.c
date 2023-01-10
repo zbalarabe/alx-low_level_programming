fb570eeba7fdf903481bf533abc8be4e8695df4f
“Zaharaddeen <zaharaddeenb@gmail.com>
2023-01-10 20:49:10 +0000

    Project functions added,

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - change lower to uppercase
+ * @s: string
+ * Return: string
+ */
+
+char *string_toupper(char *s)
+{
+
+	int i;
+
+i = 0;
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) -= 'a' - 'A';
+		i++;
+	}
+	return (s);
+}
